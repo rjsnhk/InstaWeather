08047e9a480044e5472919649a03bc3228b2c317
rjsnhk <nahakrajesh003@gmail.com>
2025-03-30 02:37:03 +0530

    update in env

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "120ef4a576b0182f0cfa5e34b9294d8e",
+  key: process.env.REACT_APP_OPENAPI_KEY,
   base: "https://api.openweathermap.org/data/2.5/",
 };
